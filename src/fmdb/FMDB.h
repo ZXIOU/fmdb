517c0b4d3fe60c846b60b89ac17b63d494165f64
zxiou <zxalcx@gmail.com>
2016-05-08 00:03:13 +0800

    Modify some of the inappropriate

--- src/fmdb/FMDB.h
+++ src/fmdb/FMDB.h
@@ -3,22 +3,13 @@
 FOUNDATION_EXPORT double FMDBVersionNumber;
 FOUNDATION_EXPORT const unsigned char FMDBVersionString[];
 
-#import "FMDatabase.h"
-#import "FMResultSet.h"
-#import "FMDatabaseAdditions.h"
-#import "FMDatabaseQueue.h"
-#import "FMDatabasePool.h"
+@class FMDatabaseQueue;
 
 @interface FMDB : NSObject
 
 @property(nonatomic, strong)FMDatabaseQueue *queue;
 
 + (instancetype)shareInstance;
-
-- (void)execSqlInFmdb:(void (^)())block;
-- (void)creatTable:(NSString *)tableName withSql:(NSString *)sql;
-- (void)insertDataIntoDataBase:(NSString *)tableName withSql:(NSString *)sql;
-- (void)deleteDataFromDataBase:(NSString *)tableName withSql:(NSString *)sql;
-- (void)updateDataFromDataBase:(NSString *)tableName withSql:(NSString *)sql;
++ (instancetype)databaseQueueSingletonWithPath:(NSString*)aPath;
 
 @end
